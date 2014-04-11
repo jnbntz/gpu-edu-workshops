febdd5279467ef5475e39be84f75d5a362c21e13
Jonathan Bentz <jbentz@nvidia.com>
2014-04-11 13:32:00 -0700

    fixing simple_add

--- exercises/cuda/simple_add/kernel.cu
+++ exercises/cuda/simple_add/kernel.cu
@@ -14,55 +14,65 @@
  *  limitations under the License.
  */
 
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
 #include <stdio.h>
 
-#define CUDA_ERROR() printf("cuda error is %s\n",cudaGetErrorString( cudaGetLastError() ));
+#ifdef DEBUG
+#define CUDA_CALL(F)  if( (F) != cudaSuccess ) \
+  {printf("Error %s at %s:%d\n", cudaGetErrorString(cudaGetLastError()), \
+   __FILE__,__LINE__); exit(-1);} 
+#define CUDA_CHECK()  if( (cudaPeekAtLastError()) != cudaSuccess ) \
+  {printf("Error %s at %s:%d\n", cudaGetErrorString(cudaGetLastError()), \
+   __FILE__,__LINE__-1); exit(-1);} 
+#else
+#define CUDA_CALL(F) (F)
+#define CUDA_CHECK() 
+#endif
 
 __global__ void add(int *a, int *b, int *c)
 {
-    *c = *a + *b;
+  *c = *a + *b;
 }
 
 int main()
 {
-    int a, b, c;
-	int *d_a, *d_b, *d_c;
-	int size = sizeof( int );
+  int a, b, c;
+  int *d_a, *d_b, *d_c;
+  int size = sizeof( int );
 
-	/* allocate space for device copies of a, b, c */
+/* allocate space for device copies of a, b, c */
 
-	cudaMalloc( (void **) &d_a, size );
-	/* enter code here to malloc d_b and d_c */
-        FIXME
+  CUDA_CALL( cudaMalloc( (void **) &d_a, size ) );
+/* enter code here to malloc d_b and d_c */
+  FIXME
 
-	/* setup initial values */
+/* setup initial values */
 
-	a = 2;
-	b = 7;
-	c = -99;
+  a = 2;
+  b = 7;
+  c = -99;
 
-	/* copy inputs to device */
+/* copy inputs to device */
 
-	cudaMemcpy( d_a, &a, size, cudaMemcpyHostToDevice );
-	/* enter code here to copy d_b to device */
-        FIXME
+  CUDA_CALL( cudaMemcpy( d_a, &a, size, cudaMemcpyHostToDevice ) );
+/* enter code here to copy d_b to device */
+  FIXME
 
-	/* launch the kernel on the GPU */
-	/* enter code here */
-        FIXME
+/* enter code here to launch the kernel on the GPU */
+  FIXME
 
-	/* copy result back to host */
+/* copy result back to host */
 
-	cudaMemcpy( &c, d_c, size, cudaMemcpyDeviceToHost );
+  CUDA_CALL( cudaMemcpy( &c, d_c, size, cudaMemcpyDeviceToHost ) );
 
-	printf("value of c after kernel is %d\n",c);
+  printf("value of c after kernel is %d\n",c);
 
-	/* clean up */
+/* clean up */
 
-	cudaFree( d_a );
-	/* enter code here to cudaFree the d_b and d_c pointers */
+  CUDA_CALL( cudaFree( d_a ) );
+/* enter code here to cudaFree the d_b and d_c pointers */
+
+/* calling reset to check errors */
+  CUDA_CALL( cudaDeviceReset() );
 	
-	return 0;
+  return 0;
 } /* end main */
