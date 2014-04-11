f9a76efa74bb315d0897a777d8425da8aeca1365
Jonathan Bentz <jbentz@nvidia.com>
2014-04-11 14:43:34 -0700

    adding error checking

--- exercise_solutions/cuda/matmul_GPU_naive/kernel.cu
+++ exercise_solutions/cuda/matmul_GPU_naive/kernel.cu
@@ -14,6 +14,9 @@
  *  limitations under the License.
  */
 
+#include <stdio.h>
+#include "cublas_v2.h"
+
 #ifdef DEBUG
 #define CUDA_CALL(F)  if( (F) != cudaSuccess ) \
   {printf("Error %s at %s:%d\n", cudaGetErrorString(cudaGetLastError()), \
@@ -26,9 +29,6 @@
 #define CUDA_CHECK() 
 #endif
 
-#include <stdio.h>
-#include "cublas_v2.h"
-
 /* macro for index calculations */
 
 #define INDX( row, col, ld ) ( ( (col) * (ld) ) + (row) )
@@ -36,8 +36,8 @@
 /* matrix size and thread dimensions */
 
 #define SIZE 1024
-#define THREAD_X 16
-#define THREAD_Y 16
+#define THREADS_PER_BLOCK_X 16
+#define THREADS_PER_BLOCK_Y 16
 
 /* naive GPU kernel where each element of C is computed by a single thread */
 
@@ -46,17 +46,20 @@ __global__ void GPU_naive( const int m, double const * const a, double const * c
 
 /* determine my threads's row and col indices in the global C matrix */
 
-	const int myrow = blockDim.x * blockIdx.x + threadIdx.x;
-	const int mycol = blockDim.y * blockIdx.y + threadIdx.y;
+  const int myrow = blockDim.x * blockIdx.x + threadIdx.x;
+  const int mycol = blockDim.y * blockIdx.y + threadIdx.y;
 
 /* if my row and col are in the C matrix, then calculate that value of C */
 
-	if( myrow < m && mycol < m )
-	{
-		register double temp = 0.0;
-		for( int k = 0; k < m; k++ ) temp += a[INDX( myrow, k, m )] * b[INDX( k, mycol, m )];
-		c[INDX( myrow, mycol, m )] = temp;
-	} /* end if */
+  if( myrow < m && mycol < m )
+  {
+    register double temp = 0.0;
+
+    for( int k = 0; k < m; k++ ) 
+      temp += a[INDX( myrow, k, m )] * b[INDX( k, mycol, m )];
+
+    c[INDX( myrow, mycol, m )] = temp;
+  } /* end if */
 
 	return;
 } /* end GPU_naive */
@@ -64,177 +67,176 @@ __global__ void GPU_naive( const int m, double const * const a, double const * c
 int main( int argc, char *argv[] )
 {
 
-    const int size = SIZE;
+  const int size = SIZE;
 
-    fprintf(stdout, "Matrix size is %d\n",size);
+  fprintf(stdout, "Matrix size is %d\n",size);
 
-    double *h_a, *h_b, *h_c, *h_c1;
-    double *d_a, *d_b, *d_c;
+  double *h_a, *h_b, *h_c, *h_c1;
+  double *d_a, *d_b, *d_c;
  
-    size_t numbytes = (size_t ) size * (size_t ) size * sizeof( double );
+  size_t numbytes = (size_t ) size * (size_t ) size * sizeof( double );
 
-    h_a = (double *) malloc( numbytes );
-    if( h_a == NULL )
-    {
-      fprintf(stderr,"Error in host malloc\n");
-      return 911;
-    }
+  h_a = (double *) malloc( numbytes );
+  if( h_a == NULL )
+  {
+    fprintf(stderr,"Error in host malloc\n");
+    return 911;
+  }
 
-    h_b = (double *) malloc( numbytes );
-    if( h_b == NULL )
-    {
-      fprintf(stderr,"Error in host malloc\n");
-      return 911;
-    }
+  h_b = (double *) malloc( numbytes );
+  if( h_b == NULL )
+  {
+    fprintf(stderr,"Error in host malloc\n");
+    return 911;
+  }
 
-    h_c = (double *) malloc( numbytes );
-    if( h_c == NULL )
-    {
-      fprintf(stderr,"Error in host malloc\n");
-      return 911;
-    }
+  h_c = (double *) malloc( numbytes );
+  if( h_c == NULL )
+  {
+    fprintf(stderr,"Error in host malloc\n");
+    return 911;
+  }
 
-    h_c1 = (double *) malloc( numbytes );
-    if( h_c1 == NULL )
-    {
-      fprintf(stderr,"Error in host malloc\n");
-      return 911;
-    }
+  h_c1 = (double *) malloc( numbytes );
+  if( h_c1 == NULL )
+  {
+    fprintf(stderr,"Error in host malloc\n");
+    return 911;
+  }
 
-	/* zero out the host memory for C matrices */
+/* zero out the host memory for C matrices */
 
-    memset( h_c, 0, numbytes );
-    memset( h_c1, 0, numbytes );
+  memset( h_c, 0, numbytes );
+  memset( h_c1, 0, numbytes );
 
-    fprintf( stdout, "Total memory required is %lf MB\n", 
-       3.0 * (double) numbytes / 1000000.0 );
+  fprintf( stdout, "Total memory required is %lf MB\n", 
+     3.0 * (double) numbytes / 1000000.0 );
 
-	/* initialize the A and B matrices */
+/* initialize the A and B matrices */
 
-    for( int i = 0; i < size * size; i++ )
-    {
-      h_a[i] = double( rand() ) / ( double(RAND_MAX) + 1.0 );
-      h_b[i] = double( rand() ) / ( double(RAND_MAX) + 1.0 );
-    }
+  for( int i = 0; i < size * size; i++ )
+  {
+    h_a[i] = double( rand() ) / ( double(RAND_MAX) + 1.0 );
+    h_b[i] = double( rand() ) / ( double(RAND_MAX) + 1.0 );
+  }
 
-	/* allocate a, b, c in gpu memory */
+/* allocate a, b, c in gpu memory */
 
-    CUDA_CALL( cudaMalloc( (void **)&d_a, numbytes ) );
-    CUDA_CALL( cudaMalloc( (void **)&d_b, numbytes ) );
-    CUDA_CALL( cudaMalloc( (void **)&d_c, numbytes ) );
+  CUDA_CALL( cudaMalloc( (void **)&d_a, numbytes ) );
+  CUDA_CALL( cudaMalloc( (void **)&d_b, numbytes ) );
+  CUDA_CALL( cudaMalloc( (void **)&d_c, numbytes ) );
 	
-	/* copy a and b to device */
+/* copy a and b to device */
 
-    CUDA_CALL( cudaMemcpy( d_a, h_a, numbytes, cudaMemcpyHostToDevice ) );
-    CUDA_CALL( cudaMemcpy( d_b, h_b, numbytes, cudaMemcpyHostToDevice ) );
+  CUDA_CALL( cudaMemcpy( d_a, h_a, numbytes, cudaMemcpyHostToDevice ) );
+  CUDA_CALL( cudaMemcpy( d_b, h_b, numbytes, cudaMemcpyHostToDevice ) );
 
-    cublasHandle_t handle;
-    cublasCreate( &handle );
+  cublasHandle_t handle;
+  cublasCreate( &handle );
 
-    double alpha = 1.0;
-    double beta  = 0.0;
+  double alpha = 1.0;
+  double beta  = 0.0;
 
-	/* start timers */
+/* start timers */
 
-    cudaEvent_t start, stop;
-    CUDA_CALL( cudaEventCreate( &start ) );
-    CUDA_CALL( cudaEventCreate( &stop ) );
-    CUDA_CALL( cudaEventRecord( start, 0 ) );
+  cudaEvent_t start, stop;
+  CUDA_CALL( cudaEventCreate( &start ) );
+  CUDA_CALL( cudaEventCreate( &stop ) );
+  CUDA_CALL( cudaEventRecord( start, 0 ) );
 
-	/* call CUBLAS dgemm */
+/* call CUBLAS dgemm */
 
-    cublasDgemm( handle, CUBLAS_OP_N, CUBLAS_OP_N,
-                 size, size, size,
-                 &alpha, 
-                 d_a, size,
-                 d_b, size,
-                 &beta,
-                 d_c, size );
+  cublasDgemm( handle, CUBLAS_OP_N, CUBLAS_OP_N,
+               size, size, size,
+               &alpha, 
+               d_a, size,
+               d_b, size,
+               &beta,
+               d_c, size );
 
-	/* stop timers */
+/* stop timers */
 
-    CUDA_CALL( cudaEventRecord( stop, 0 ) );
-    CUDA_CALL( cudaEventSynchronize( stop ) );
-    float elapsedTime;
-    CUDA_CALL( cudaEventElapsedTime( &elapsedTime, start, stop ) );
+  CUDA_CALL( cudaEventRecord( stop, 0 ) );
+  CUDA_CALL( cudaEventSynchronize( stop ) );
+  float elapsedTime;
+  CUDA_CALL( cudaEventElapsedTime( &elapsedTime, start, stop ) );
 
-	/* print GPU CUBLAS timing information */
+/* print GPU CUBLAS timing information */
 
-    fprintf(stdout, "Total time GPU CUBLAS is %f sec\n", elapsedTime / 1000.0f );
-    fprintf(stdout, "Performance is %f GFlop/s\n", 
-      2.0 * (double) size * (double) size * (double) size / 
-      ( (double) elapsedTime / 1000.0 ) * 1.e-9 );
+  fprintf(stdout, "Total time GPU CUBLAS is %f sec\n", elapsedTime / 1000.0f );
+  fprintf(stdout, "Performance is %f GFlop/s\n", 
+    2.0 * (double) size * (double) size * (double) size / 
+    ( (double) elapsedTime / 1000.0 ) * 1.e-9 );
     
-	/* copy C from device to host for error checking */
+/* copy C from device to host for error checking */
 
-    CUDA_CALL( cudaMemcpy( h_c, d_c, numbytes, cudaMemcpyDeviceToHost ) );
+  CUDA_CALL( cudaMemcpy( h_c, d_c, numbytes, cudaMemcpyDeviceToHost ) );
 
-	/* reset C on device to zero */
+/* reset C on device to zero */
 
-	cudaMemset( d_c, 0, numbytes );
+  CUDA_CALL( cudaMemset( d_c, 0, numbytes ) );
 
-	/* setup grid and block sizes */
+/* setup grid and block sizes */
 
-	dim3 threads( THREAD_X, THREAD_Y, 1 );
-	dim3 blocks( size / THREAD_X + 1, size / THREAD_Y + 1, 1 );
+  dim3 threads( THREADS_PER_BLOCK_X, THREADS_PER_BLOCK_Y, 1 );
+  dim3 blocks( size / THREADS_PER_BLOCK_X + 1, 
+               size / THREADS_PER_BLOCK_Y + 1, 1 );
 
-	/* start timers */
+/* start timers */
 
-     CUDA_CALL( cudaEventRecord( start, 0 ) );
+  CUDA_CALL( cudaEventRecord( start, 0 ) );
 
-	/* call GPU_naive */
+/* call GPU_naive */
 
-     GPU_naive<<< blocks, threads >>> ( size, d_a, d_b, d_c );
-     CUDA_CHECK()
-#ifdef DEBUG
-     CUDA_CALL( cudaDeviceSynchronize() );
-#endif
+  GPU_naive<<< blocks, threads >>> ( size, d_a, d_b, d_c );
+  CUDA_CHECK()
+  CUDA_CALL( cudaDeviceSynchronize() );
 
-	/* stop timers */
+/* stop timers */
 
-    CUDA_CALL( cudaEventRecord( stop, 0 ) );
-    CUDA_CALL( cudaEventSynchronize( stop ) );
-    CUDA_CALL( cudaEventElapsedTime( &elapsedTime, start, stop ) );
+  CUDA_CALL( cudaEventRecord( stop, 0 ) );
+  CUDA_CALL( cudaEventSynchronize( stop ) );
+  CUDA_CALL( cudaEventElapsedTime( &elapsedTime, start, stop ) );
 
-	/* print data for GPU naive */
+/* print data for GPU naive */
 
-    fprintf(stdout, "Total time GPU NAIVE is %f sec\n", elapsedTime / 1000.0f );
-    fprintf(stdout, "Performance is %f GFlop/s\n", 
-      2.0 * (double) size * (double) size * (double) size / 
-      ( (double) elapsedTime / 1000.0 ) * 1.e-9 );
+  fprintf(stdout, "Total time GPU NAIVE is %f sec\n", elapsedTime / 1000.0f );
+  fprintf(stdout, "Performance is %f GFlop/s\n", 
+    2.0 * (double) size * (double) size * (double) size / 
+    ( (double) elapsedTime / 1000.0 ) * 1.e-9 );
                   
-	/* copy C back to host */
+/* copy C back to host */
 	
-	cudaMemcpy( h_c1, d_c, numbytes, cudaMemcpyDeviceToHost );
+  CUDA_CALL( cudaMemcpy( h_c1, d_c, numbytes, cudaMemcpyDeviceToHost ) );
 
-    cublasDestroy( handle );
-    CUDA_CALL( cudaEventDestroy( start ) );
-    CUDA_CALL( cudaEventDestroy( stop ) );
+  cublasDestroy( handle );
+  CUDA_CALL( cudaEventDestroy( start ) );
+  CUDA_CALL( cudaEventDestroy( stop ) );
 
-	/* check CUBLAS versus GPU NAIVE numerical results */
+/* check CUBLAS versus GPU NAIVE numerical results */
 
-	double temp = 0.0;
+  double temp = 0.0;
 
-	for( int i = 0; i < size * size; i++ )
-	{
-		temp += ( h_c[i] - h_c1[i] ) * ( h_c[i] - h_c1[i] );
-	} /* end for */
+  for( int i = 0; i < size * size; i++ )
+  {
+    temp += ( h_c[i] - h_c1[i] ) * ( h_c[i] - h_c1[i] );
+  } /* end for */
 
-	printf("error is %f\n",temp);
-	if( temp > 10 ) printf("Error value is suspiciously high!\n");
+  printf("error is %f\n",temp);
+  if( temp > 10 ) printf("Error value is suspiciously high!\n");
 
-	/* cleanup */
+/* cleanup */
 
-    CUDA_CALL( cudaFree( d_a ) );
-    CUDA_CALL( cudaFree( d_b ) );
-    CUDA_CALL( cudaFree( d_c ) );
+  CUDA_CALL( cudaFree( d_a ) );
+  CUDA_CALL( cudaFree( d_b ) );
+  CUDA_CALL( cudaFree( d_c ) );
 
-    free( h_a );
-    free( h_b );
-    free( h_c );
-    free( h_c1 );
+  free( h_a );
+  free( h_b );
+  free( h_c );
+  free( h_c1 );
 
-    CUDA_CALL( cudaDeviceReset() );
+  CUDA_CALL( cudaDeviceReset() );
 
-    return 0;
+  return 0;
 }
