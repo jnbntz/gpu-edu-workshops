8684bc6ab6d04da8e11009642ea229e5610a18c5
Jonathan Bentz <jnbntz@gmail.com>
2017-02-28 15:18:02 -0600

    Changes for TF 1.0.0 API and better logging

--- exercises/tf/segmentation/simple/runEval.py
+++ exercises/tf/segmentation/simple/runEval.py
@@ -16,7 +16,8 @@
 
 from datetime import datetime
 import time
-import os.path
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
